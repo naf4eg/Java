c214e62a5f72d7d2956229da4333d918d51b22cd
Konstantin <naf4eg@yandex.ru>
2022-06-24 00:41:55 +0300

    stream

--- src/main/java/stream/task.java
+++ src/main/java/stream/task.java
@@ -1,7 +1,15 @@
 package stream;
 
+import java.util.stream.IntStream;
+
 public class task {
     public static void main(String[] args) {
 
     }
+
+    public static IntStream pseudoRandomStream(int seed) {
+        return null; // your implementation here
+    }
 }
+
+
