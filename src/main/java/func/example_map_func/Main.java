be22f87977bf28a39f7496641773b7314f839185
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-07-04 13:28:28 +0300

    примеры работы с map в функциональном стиле  - методы compute, computeIfAbsent, computeIfPresent

--- src/main/java/func/example_map_func/Main.java
+++ src/main/java/func/example_map_func/Main.java
@@ -2,12 +2,14 @@ package func.example_map_func;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
     public static void main (String[] args) {
         //        forEachMapExample();
         //        replaceMapExample();
        // mergeMapExample();
+        computeMapExample();
     }
 
     private static void forEachMapExample () {
@@ -31,12 +33,10 @@ public class Main {
     }
 
     private static void replaceMapExample () {
-        Map<String, String> phoneBook = new HashMap<>(Map.of("Tom Hunter",
-            "0131 128 8144",
-            "Layla Graham",
-            "0702 7296 3035",
-            "Michael Butler",
-            "0035 0938 2100"
+        Map<String, String> phoneBook = new HashMap<>(Map.of(
+            "Tom Hunter", "0131 128 8144",
+            "Layla Graham", "0702 7296 3035",
+            "Michael Butler", "0035 0938 2100"
         ));
 
         phoneBook.replaceAll((name, phone) -> "1 " + phone);
@@ -61,6 +61,106 @@ public class Main {
             company,
             countEmployee
         ));
+    }
+
+    private static void computeMapExample() {
+        Map<String, Integer> companyToNumberOfEmployeesMap = new HashMap<>();
+        companyToNumberOfEmployeesMap.put("Goods Inc", 550);
+        companyToNumberOfEmployeesMap.put("BestDevStudio", 1200);
+        companyToNumberOfEmployeesMap.put("Ggl", 1000000);
+
+        System.out.println("--------------------------");
+
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+
+        companyToNumberOfEmployeesMap.compute("Ggl", (company, employees) -> (int) (employees * 0.8));
+
+        System.out.println("--------------------------");
+
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+
+        System.out.println("--------------------------");
+
+        /* Когда не нашлось нужного ключа в мап, то будет исключение, тогда делаем проверку */
+        companyToNumberOfEmployeesMap.compute("Unknown",
+            (company, employees) -> Objects.nonNull(employees) ? (int) (employees * 0.8) : 0);
+
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+
+        System.out.println("--------------------------");
+
+        /* вычислить если отстутствует */
+        companyToNumberOfEmployeesMap.computeIfAbsent("NewCompany", value -> 40);
+
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+
+        System.out.println("--------------------------");
+
+        /* вычислить если отстутствует. Вычисления не происходят если ключ уже есть */
+        companyToNumberOfEmployeesMap.computeIfAbsent("NewCompany", value-> {
+            System.out.println("вычисляем! - 1");
+            return 77;
+        });
+
+        companyToNumberOfEmployeesMap.remove("NewCompany");
+        System.out.println("--------------------------");
+        System.out.println("Удаляем NewCompany");
+        System.out.println("--------------------------");
+
+        /* вычислить если отстутствует. Вычисления не происходят если ключ уже есть */
+        /* тут вычисления проходят так-как ключа нет в мап */
+        companyToNumberOfEmployeesMap.computeIfAbsent("NewCompany", value-> {
+            System.out.println("вычисляем! - 2");
+            return 77;
+        });
+
+        System.out.println("--------------------------");
+
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+        System.out.println("--------------------------");
+
+        /* вычислить если присутствует такой ключ. Вычисления происходят если ключ уже есть */
+        companyToNumberOfEmployeesMap.computeIfPresent("NewCompany", (key, value)-> {
+            System.out.println("вычисляем! - 3");
+            return 100;
+        });
+
+        System.out.println("--------------------------");
+
+        /* тут вычисления проходят так-как ключа нет в мап */
+        companyToNumberOfEmployeesMap.forEach((company, countEmployee) -> System.out.printf(
+            "Company: %s | Count Employee: %s\n",
+            company,
+            countEmployee
+        ));
+
+        System.out.println("--------------------------");
+
+        /* вычислить если присутствует такой ключ. В данно методе вычисления не происходят */
+        companyToNumberOfEmployeesMap.computeIfPresent("Kukuruma", (key, value)-> {
+            System.out.println("вычисляем! - 4");
+            return 1000;
+        });
     }
 
 }
