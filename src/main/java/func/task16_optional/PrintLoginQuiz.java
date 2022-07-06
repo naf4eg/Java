0b337f0d43817c8f16f51a9b6659d04573a4f25d
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-07-07 00:39:08 +0300

    refactoring

--- src/main/java/func/task16_optional/PrintLoginQuiz.java
+++ src/main/java/func/task16_optional/PrintLoginQuiz.java
@@ -7,7 +7,8 @@ class PrintLoginQuiz {
     public static void printLoginIfPro (Set<User> users, String id) {
         users.forEach(user -> {
             user.getAccount()
-                .filter(account -> account.getId().equals(id) && account.getType().equals("pro"))
+                .filter(account -> account.getId().equals(id))
+                .filter(account -> account.getType().equals("pro"))
                 .ifPresent(value -> System.out.println(user.getLogin()));
         });
     }
