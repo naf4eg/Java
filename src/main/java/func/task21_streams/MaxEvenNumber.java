9c8de1ba0a8346f5b9de86a4917e15cece333b97
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2022-07-17 18:51:02 +0300

    Streams filtering task 21 find Max even number

---
+++ src/main/java/func/task21_streams/MaxEvenNumber.java
@@ -0,0 +1,28 @@
+package func.task21_streams;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * You need to implement the findMaxEvenNumber method to find the maximum number that is divisible by 2 (even).
+ * The method should return the found number or 0 if the stream doesn't contain any even numbers.
+ */
+public class MaxEvenNumber {
+    public static int findMaxEvenNumber(Collection<Integer> numbers) {
+        return numbers.stream().filter(num -> num % 2 == 0).max(Comparator.naturalOrder()).orElse(0); // write your code here
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                                     .map(Integer::parseInt)
+                                     .collect(Collectors.toSet());
+
+        System.out.println(findMaxEvenNumber(numbers));
+    }
+}
